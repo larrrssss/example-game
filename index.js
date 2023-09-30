aadda377537c33c26d73c128416a39847f54c902
larrrssss <63020111+larrrssss@users.noreply.github.com>
2023-09-30 18:25:47 +0200

    test

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ async function preload() {
   return data;
 }
 
+let i = 0;
+
+function setup(preloadedData) {
+  i = preloadedData.data;
+}
+
 function loop() {
+  i++;
+
+  console.log("HITTING GAME LOOP", i % 5);
   console.log("HITTING GAME LOOP WOW");
 }
